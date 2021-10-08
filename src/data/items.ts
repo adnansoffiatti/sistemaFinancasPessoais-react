3d9617acb017c4d999f08d533cccb79948c5f4d4
Adnan Soffiatti <adnan.soffiatti@gmail.com>
2021-10-08 09:42:59 -0300

    Sistema Finanças Pessoais em React

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 6), category: 'food', title: 'Pizza', value: 65.00 },
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Burguer King', value: 28 },
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+    { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário Fixo', value: 10000 }
+];
